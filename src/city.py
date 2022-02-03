5f3f317597999747de591572418a44ec5f96d682
Natasha Moorfield <mailbox@natashamoorfield.co.uk>
2022-02-03 16:41:29 +0000

    Update City class
    The PopulationNumber class integrated into the City class.

--- src/city.py
+++ src/city.py
@@ -1,29 +1,11 @@
-from dataclasses import dataclass
-from typing import Optional
+from typing import Union
+from src.population_number import PopulationNumber
 
 
-@dataclass()
 class City(object):
     """
     The City class, unsurprisingly, holds data about Calmarendian cities.
     """
-    name: str
-    population: Optional[int] = None
-
-    def __post_init__(self):
-        """
-        Data validation
-        Population can be None (to indicate missing information) but must otherwise be
-        a non-negative integer; zero is treated as valid and indicates a city with no people.
-        """
-        if self.population is not None:
-            if not isinstance(self.population, int):
-                raise TypeError("Population must be None or a non-negative integer.")
-            if self.population < 0:
-                raise ValueError("Population must be None or a non-negative integer.")
-
-    @property
-    def population_string(self) -> str:
-        if self.population is None:
-            return "Unknown"
-        return f"{self.population:,d}"
+    def __init__(self, name: str, population: Union[int, float] = None):
+        self.name = name
+        self.population = PopulationNumber(population)
--- tests/test_city.py
+++ tests/test_city.py
@@ -10,18 +10,19 @@ class CityTest(unittest.TestCase):
 
     def test_valid_initialization(self):
         self.assertEqual(self.vienna.name, "Vienna")
-        self.assertIsNone(self.vienna.population)
+        self.assertIsNone(self.vienna.population.value)
         self.assertEqual(self.port_victoria.name, "Port Victoria")
-        self.assertEqual(self.port_victoria.population, 1200000)
-        self.assertEqual(self.deserted_city.population, 0)
+        self.assertEqual(self.port_victoria.population.value, 1200000)
+        self.assertEqual(self.deserted_city.population.value, 0)
+        c17 = City("City 17", 10.75)
+        self.assertEqual(c17.population.value, 11)
 
     def test_calculated_data(self):
-        self.assertEqual(self.vienna.population_string, "Unknown")
-        self.assertEqual(self.port_victoria.population_string, "1,200,000")
+        self.assertEqual(self.vienna.population.as_string(), "unknown")
+        self.assertEqual(self.port_victoria.population.as_string(), "1,200,000")
 
     def test_duff_population(self):
         self.assertRaises(ValueError, City, "Maryport", -10)
-        self.assertRaises(TypeError, City, "Maryport", 10.5)
 
 
 if __name__ == '__main__':
